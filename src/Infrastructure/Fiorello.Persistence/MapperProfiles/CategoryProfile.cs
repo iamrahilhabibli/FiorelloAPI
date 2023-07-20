b9fc211c1e413ca9a9a5b769d61f4aa167b83915
leylam22 <leylarm@code.edu.az>
2023-07-21 03:26:54 +0400

    added Auto Mapper

---
+++ src/Infrastructure/Fiorello.Persistence/MapperProfiles/CategoryProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Fiorello.Application.DTOs.CategoryDTOs;
+using Fiorello.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiorello.Persistence.MapperProfiles;
+
+public class CategoryProfile:Profile
+{
+	public CategoryProfile()
+	{
+		CreateMap<Category, CategoryCreateDto>().ReverseMap();
+		CreateMap<Category, CategoryGetDto>().ReverseMap();
+		CreateMap<Category, CategoryUpdateDto>().ReverseMap();
+	}
+}
