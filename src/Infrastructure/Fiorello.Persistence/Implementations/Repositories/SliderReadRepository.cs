1f0e424744fc1f5f0d603c8aaae76df2c3fa44b9
leylam22 <leylarm@code.edu.az>
2023-07-25 03:29:00 +0400

    Added Slider service

---
+++ src/Infrastructure/Fiorello.Persistence/Implementations/Repositories/SliderReadRepository.cs
@@ -0,0 +1,12 @@
+﻿using Fiorello.Application.Abstraction.Repository;
+using Fiorello.Domain.Entities;
+using Fiorello.Persistence.Contexts;
+
+namespace Fiorello.Persistence.Implementations.Repositories;
+
+public class SliderReadRepository : ReadRepository<Slider>, ISliderReadRepository
+{
+    public SliderReadRepository(AppDbContext context) : base(context)
+    {
+    }
+}
