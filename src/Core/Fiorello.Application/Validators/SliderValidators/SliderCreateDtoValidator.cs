cafef92b1e74e41899e2d019123df7486055f2c5
leylam22 <leylarm@code.edu.az>
2023-07-25 02:09:29 +0400

    Added Slider Create Dto Validator

---
+++ src/Core/Fiorello.Application/Validators/SliderValidators/SliderCreateDtoValidator.cs
@@ -0,0 +1,14 @@
+﻿using Fiorello.Application.DTOs.SliderDTOs;
+using FluentValidation;
+
+namespace Fiorello.Application.Validators.SliderValidators;
+
+public class SliderCreateDtoValidator : AbstractValidator<SliderCreateDto>
+{
+	public SliderCreateDtoValidator()
+	{
+		RuleFor(s => s.title).NotNull().NotEmpty().MaximumLength(50);
+		RuleFor(s => s.description).NotNull().NotEmpty().MaximumLength(150);
+		RuleFor(s=>s.imagePath).NotNull().NotEmpty().MaximumLength(255);
+	}
+}
--- src/Core/Fiorello.Domain/Entities/Slider.cs
+++ src/Core/Fiorello.Domain/Entities/Slider.cs
@@ -4,7 +4,7 @@ namespace Fiorello.Domain.Entities;
 
 public class Slider:BaseEntity
 {
-    public string Title { get; set; }
-    public string Description { get; set; }
-    public string ImagePath { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public string? ImagePath { get; set; }
 }
